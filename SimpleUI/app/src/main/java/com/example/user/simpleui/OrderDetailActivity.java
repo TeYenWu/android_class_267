6ab0e1e8145fe50ad7d8e516ea6c0c2d35584427
Wuteyan <killerw74@gmail.com>
2016-06-27 21:22:20 +0800

    get LatLng from address

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,11 +1,23 @@
 package com.example.user.simpleui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class OrderDetailActivity extends AppCompatActivity {
+
+    TextView noteTextView;
+    TextView menuResultsTextView;
+    TextView storeInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +29,56 @@ public class OrderDetailActivity extends AppCompatActivity {
         String menuResults = intent.getStringExtra("menuResults");
         String storeInfo = intent.getStringExtra("storeInfo");
 
-        Log.d("debug", note);
-        Log.d("debug", menuResults);
-        Log.d("debug", storeInfo);
+        noteTextView = (TextView) findViewById(R.id.noteTextView);
+        storeInfoTextView = (TextView) findViewById(R.id.storeInfoTextView);
+        menuResultsTextView = (TextView) findViewById(R.id.menuResultsTextView);
 
+        if (note != null)
+            noteTextView.setText(note);
+
+        if (storeInfo != null)
+            storeInfoTextView.setText(storeInfo);
+
+        String text = "";
+        if (menuResults != null) {
+            try {
+                JSONArray jsonArray = new JSONArray(menuResults);
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject object = jsonArray.getJSONObject(i);
+                    String lNumber = String.valueOf(object.getInt("lNumber"));
+                    String mNumber = String.valueOf(object.getInt("mNumber"));
+                    text += object.getString("drinkName") + " : 大杯 " + lNumber + "杯      中杯" + mNumber + "杯 \n";
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        menuResultsTextView.setText(text);
+
+        String[] storeInfos = storeInfo.split(",");
+        if(storeInfos != null && storeInfos.length > 1)
+        {
+            String address = storeInfos[1];
+            (new GeoCodingTask()).execute(address);
+        }
+    }
+
+    private static class GeoCodingTask extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            String address = params[0];
+            double[] latlng = Utils.getLatLngFromGoogleMapAPI(address);
+            if(latlng != null) {
+                Log.d("Debug", String.valueOf(latlng[0]));
+                Log.d("Debug", String.valueOf(latlng[1]));
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+        }
     }
 }
